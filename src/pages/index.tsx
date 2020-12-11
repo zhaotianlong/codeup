866aa718eb93dcfd00e03604a64dcea9c16eab1a
zhaotianlong <zhaotianlong@tungee.com>
2020-12-11 16:49:59 +0800

    trst

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useHistory } from 'umi';
 import styles from './index.less';
 
 export default () => {
+  const history = useHistory();
   return (
     <div>
+      <button onClick={() => history.push('/a')}>tttttt</button>
       <h1 className={styles.title}>Page index</h1>
     </div>
   );
-}
+};
