7e09b242a1ce7d46e4cb2a64f3347555a489405b
zhaotianlong <zhaotianlong@tungee.com>
2020-12-11 16:38:11 +0800

    trst

---
+++ src/pages/a/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+export default () => {
+  useEffect(() => {
+    debugger;
+  }, []);
+  return (
+    <div>
+      <h1>Page index tesss</h1>
+    </div>
+  );
+};
